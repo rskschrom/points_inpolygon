1eb56b7852697ecc9ce2aab25c4af1ce9043828d
Robert Schrom <rss5116@psu.edu>
2017-06-15 17:17:32 -0400

    update

--- segments.py
+++ segments.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 # create random line segments
 numseg = 10
-seg_width = np.random.rand(numseg)*0.2+0.1
+seg_width = np.random.rand(numseg)*0.1+0.1
 seg_str = np.random.rand(numseg)*0.7
 seg_end = seg_str+seg_width
 
@@ -16,10 +16,21 @@ rcomp = seg_end[lcomp_ind]
 print lcomp, rcomp
 
 # find left edges >= lcomp and <= rcomp
-lcontain_inds = [(seg_str>=lcomp)&(seg_str<=rcomp)]
-rgt_contain = seg_end[lcontain_inds]
-rcomp = np.max(rgt_contain)
-print lcomp, rcomp
+ljmax = lcomp
+rjmax = rcomp
+num_overlap = 2
+
+while (num_overlap>1):
+    j_inds = [(seg_str>=ljmax)&(seg_str<=rjmax)&(seg_end>=rjmax)]
+    lj = seg_str[j_inds]
+    rj = seg_end[j_inds]
+    num_overlap = len(lj)
+    jmax = np.argmax(rj)
+    ljmax = lj[jmax]
+    rjmax = rj[jmax]
+    print num_overlap, lcomp, ljmax, rjmax
+
+rcomp = rjmax
 
 for i in range(numseg):
     # plot

59f8cfcc3543aaf59b9059820d72cfc93ce3e857
Robert Schrom <snowwxradar@users.noreply.github.com>
2017-06-16 16:41:16 -0400

    made overlap bound indicator a function

--- segments.py
+++ segments.py
@@ -3,6 +3,27 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
+# function to get bounds of continuous overlapping lines
+def get_overlap_bounds(seg_mins, seg_maxs, min_init, max_init):
+    # find left edges of segments >= min_init and <= max_init
+    min_jmax = min_init
+    max_jmax = max_init
+    num_overlap = 2
+
+    while (num_overlap>1):
+        j_inds = [(seg_str>=min_jmax)&(seg_str<=max_jmax)&(seg_end>=max_jmax)]
+        min_j = seg_str[j_inds]
+        max_j = seg_end[j_inds]
+        num_overlap = len(min_j)
+        jmax = np.argmax(max_j)
+        min_jmax = min_j[jmax]
+        max_jmax = max_j[jmax]
+        print num_overlap, min_init, min_jmax, max_jmax
+
+    min_overlap = min_init
+    max_overlap = max_jmax
+    return min_overlap, max_overlap
+
 # create random line segments
 numseg = 10
 seg_width = np.random.rand(numseg)*0.1+0.1
@@ -16,21 +37,15 @@ rcomp = seg_end[lcomp_ind]
 print lcomp, rcomp
 
 # find left edges >= lcomp and <= rcomp
-ljmax = lcomp
-rjmax = rcomp
-num_overlap = 2
+lcomps = np.ma.masked_all([numseg])
+rcomps = np.ma.masked_all([numseg])
+lcomp, rcomp = get_overlap_bounds(seg_str, seg_end, lcomp, rcomp)
+print lcomp, rcomp
 
-while (num_overlap>1):
-    j_inds = [(seg_str>=ljmax)&(seg_str<=rjmax)&(seg_end>=rjmax)]
-    lj = seg_str[j_inds]
-    rj = seg_end[j_inds]
-    num_overlap = len(lj)
-    jmax = np.argmax(rj)
-    ljmax = lj[jmax]
-    rjmax = rj[jmax]
-    print num_overlap, lcomp, ljmax, rjmax
-
-rcomp = rjmax
+# determine if there are additional segments to be analyzed
+add_seg_inds = np.arange(numseg)[seg_str>rcomp]
+num_add_seg = len(add_seg_inds)
+print add_seg_inds, num_add_seg
 
 for i in range(numseg):
     # plot
